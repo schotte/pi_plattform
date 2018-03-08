b328d923c53c745f04b2d6d384da2b19e3ccc3dc
Matthias Jakob <schotte93@googlemail.com>
2018-03-08 18:09:49 +0100

    first version of hc-sr04 sensor

---
+++ src/HC-SR04_node.cpp
@@ -0,0 +1,50 @@
+/*
+ * HC-SR04_node.cpp
+ *
+ *  Created on: Mar 4, 2018
+ *      Author: schotte
+ */
+
+#include "ros/ros.h"
+#include "sensor_msgs/Range.h"
+#include "wiringPi.h"
+#include "libSonar.h"
+
+int main(int argc, char **argv) {
+
+	ros::init(argc, argv, "motor");
+	ros::NodeHandle n("~");
+	wiringPiSetup();
+
+	int pinEcho, pinTrig;
+	if (!n.getParam("pinEcho", pinEcho)) {
+		ROS_WARN("No Pin supplied(A)");
+		return 1;
+	}
+	if (!n.getParam("pinTrig", pinTrig)) {
+		ROS_WARN("No Pin supplied(B)");
+		return 1;
+	}
+
+	ros::Rate loop_rate(25);
+
+	ros::Publisher pub = n.advertise<sensor_msgs::Range>("range", 1);
+
+	Sonar sonar;
+	sonar.init(pinTrig, pinEcho);
+	sensor_msgs::Range msg;
+	msg.header.frame_id = "somethign";
+	msg.radiation_type=0;
+	msg.min_range = 0.002;
+	msg.max_range = 0.450;
+	msg.field_of_view = 0.261799;
+
+	while(ros::ok()) {
+		msg.range = sonar.distance(30000);
+		msg.header.stamp = ros::Time::now();
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+
+}
